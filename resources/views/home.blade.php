12bb0d43f0693955a600dee2777c40ec28d2229a
drklrd <srvbhattarai@gmail.com>
2017-07-08 21:09:45 +0545

    add user form validation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,11 +6,20 @@
 		<a href="{{ route('niceaction',['action'=>'hi']) }}">Hi</a>
 		<a href="{{ route('niceaction',['action'=>'hello']) }}">Hello</a>
 		<br>
+		@if (count($errors) > 0)
+			<div>
+				<ul>
+					@foreach($errors->all() as $error)
+						{{ $error }}
+					@endforeach
+				</ul>
+			</div>
+		@endif
 		<form action="{{ route('formsubmit') }}" method="post">
 			<label for="select-action">I want to </label>
 			<select name="action" id="select-action">
 				<option value="greet">Greet</option>
-				<option value="hi">Hi</option>
+				<option value="hi">Hi</option> 
 				<option value="hello">Hello</option>
 			</select>
 			<input type="text" name="name">
--- routes/web.php
+++ routes/web.php
@@ -11,19 +11,22 @@
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
 
-Route::group(['prefix' => 'do'],function(){
-	Route::get('/{action}/{name?}',[
-		'uses' => 'NiceActionController@getNiceAction',
-		'as' => 'niceaction'
-	]);
 
-	Route::post('/',[
-		'uses' => 'NiceActionController@postNiceAction',
-		'as' => 'formsubmit'
-	]);
+Route::group(['middleware' => ['web']],function(){
+	Route::get('/', function () {
+	    return view('home');
+	})->name('home');
+
+	Route::group(['prefix' => 'do'],function(){
+		Route::get('/{action}/{name?}',[
+			'uses' => 'NiceActionController@getNiceAction',
+			'as' => 'niceaction'
+		]);
+
+		Route::post('/',[
+			'uses' => 'NiceActionController@postNiceAction',
+			'as' => 'formsubmit'
+		]);
+	});
 });
-
